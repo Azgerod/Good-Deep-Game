e2f1060334343d1c7c15ae5cebd49b4ed4f5e9a3
Cuyler Brehaut <cy378850@dal.ca>
2024-05-03 14:56:13 -0300

    Added 'new game' and 'resume' contexts and band-aided circular referencing.

--- spine.py
+++ spine.py
@@ -2,47 +2,59 @@ import os
 
 
 class Context:
-    def __init__(self, description, command_intro, commands, prompt):
+    def __init__(self, description, command_intro, prompt):
         self.description = description
         self.command_intro = command_intro
-        self.commands = commands
-        self.commands['exit'] = exit
+        self.commands = {'exit': exit}
         self.prompt = prompt
 
+    def set_commands(self, commands):
+        self.commands.clear()
+        self.commands.update(commands)
+        self.commands['exit'] = exit
 
     def command_list(self):
         phrases = self.commands.keys()
-        return 'Enter a command.\n' + '\n'.join(phrases)
+        return '\n'.join(phrases)
 
     def __str__(self):
-        return self.description + 2*'\n' + self.command_intro + '\n' + self.command_list()
+        return self.description + 2 * '\n' + self.command_intro + '\n' + self.command_list()
 
     def request(self):
         return input('\n' + self.prompt + ' ')
-    
+
     def execute(self, phrase):
         executor = self.commands[phrase]
         executor()
 
     def __call__(self):
-        os.system('cls' if os.name == 'nt' else 'clear')
+        os.system('cls' or 'clear')
         print(self)
-        while True:
-            self.execute(self.request())
+        self.execute(self.request())
 
 
-# Command definitions
-def new_game():
-    print('Yep this is the game.')
-
-def resume():
-    print('Resuming :)')
-
-mm_description = "Welcome to our game."
-mm_command_intro = "Enter one of the following commands."
-mm_commands = {'new': new_game, 'resume': resume}
+# Main menu context
+mm_description = 'Welcome to the Main Menu context.'
+mm_command_intro = 'Enter one of the following commands.'
 mm_prompt_string = '>'
+main_menu = Context(mm_description, mm_command_intro, mm_prompt_string)
 
-main_menu = Context(mm_description, mm_command_intro, mm_commands, mm_prompt_string)
+# New game context
+ng_description = 'You have entered the New Game context.'
+ng_command_intro = "Return to the menu when you're ready."
+ng_prompt_string = '|>'
+new_game = Context(ng_description, ng_command_intro, ng_prompt_string)
 
+# Resume context
+r_description = 'You have resumed.'
+r_command_intro = 'Nothing to see here.'
+r_prompt_string = '->>'
+resume = Context(r_description, r_command_intro, r_prompt_string)
+
+# Command lists:
+main_menu.set_commands({'new': new_game, 'resume': resume})
+new_game.set_commands({'menu': main_menu})
+resume.set_commands({'menu': main_menu})
+
+# Run the game
 main_menu()
